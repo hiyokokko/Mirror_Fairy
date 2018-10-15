caca3b390a778920fd4b983750ae8c0f0016e50f
hiyokokkoLLS <hiyokokko65535@gmail.com>
2018-10-15 11:42:27 +0900

    不要な関数、画像の削除。

--- Mirror_Fairies/Assets/Scripts/Conversion.cs
+++ Mirror_Fairies/Assets/Scripts/Conversion.cs
@@ -1,9 +1,5 @@
 ﻿public class Conversion
 {
-	public static float FloatConversion(double d)
-	{
-		return float.Parse(d.ToString());
-	}
 	public static double DoubleConversion(float f)
 	{
 		return double.Parse(f.ToString());
