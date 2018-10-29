71eeb8596ab4bf2f6db2288c750b838569c49893
hiyokokkoLLS <hiyokokko65535@gmail.com>
2018-10-29 12:32:51 +0900

    HardBlack(自キャラ)の作成

--- Mirror_Fairies/Assets/Fairies/Hard/Black/HardBlack.cs
+++ Mirror_Fairies/Assets/Fairies/Hard/Black/HardBlack.cs
@@ -1,16 +1,84 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class HardBlack : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
+﻿using UnityEngine;
+public class HardBlack : MonoBehaviour
+{
+	[SerializeField] GameObject burret;
+	PlayerTouchState playerTouchState;
+	float attackWait;
+	float attackTime;
+	int burretNum;
+	float burretTargetX;
+	float burretSpeed;
+	float moveRestRight;
+	float moveRestOther;
+	void Start()
+	{
+		PlayerOperation.cam = GameObject.Find("Camera").GetComponent<Camera>();
+		playerTouchState = new PlayerTouchState(-1, -1);
+		attackWait = 0.1f;
+		attackTime = attackWait;
+		burretNum = 5;
+		burretTargetX = 13.0f;
+		burretSpeed = 16.0f;
+		moveRestRight = 3.0f;
+		moveRestOther = 1.0f;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	void Update()
+	{
+		playerTouchState = PlayerOperation.PlayerTouch(playerTouchState, transform.position);
+		if (attackTime < attackWait) { attackTime += Time.deltaTime; }
+		if (playerTouchState.attack != -1 && attackTime >= attackWait) { Attack(); }
+		if (playerTouchState.move != -1) { Move(); }
+	}
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		AudioSE.kill = true;
+		Main.gameOver = true;
+		Destroy(gameObject);
+	}
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		AudioSE.kill = true;
+		Main.gameOver = true;
+		Destroy(gameObject);
+	}
+	void Attack()
+	{
+		Vector2 burretTarget = new Vector2(burretTargetX, transform.position.y);
+		Vector2[] burretPos = new Vector2[5]
+		{
+			new Vector2(transform.position.x + 1, transform.position.y + 1.0f),
+			new Vector2(transform.position.x + 1, transform.position.y + 0.5f),
+			new Vector2(transform.position.x + 1, transform.position.y),
+			new Vector2(transform.position.x + 1, transform.position.y - 0.5f),
+			new Vector2(transform.position.x + 1, transform.position.y - 1.0f)
+		};
+		for (int i = 0; i < burretNum; i++)
+		{
+			GameObject burretInst = Instantiate(burret, burretPos[i], Quaternion.identity);
+			burretInst.GetComponent<HardBlackBurret>().target = burretTarget;
+			burretInst.GetComponent<HardBlackBurret>().speed = burretSpeed;
+		}
+		attackTime -= attackWait;
+	}
+	void Move()
+	{
+		Vector2 afterPos = playerTouchState.beforePos + (playerTouchState.afterTouchPos - playerTouchState.beforeTouchPos);
+		transform.position = afterPos;
+		if (EndChecker.EndRight(transform.position.x + moveRestRight))
+		{
+			transform.position = new Vector2(EndChecker.endRight - moveRestRight, transform.position.y);
+		}
+		else if (EndChecker.EndLeft(transform.position.x - moveRestOther))
+		{
+			transform.position = new Vector2(EndChecker.endLeft + moveRestOther, transform.position.y);
+		}
+		if (EndChecker.EndTop(transform.position.y + moveRestOther))
+		{
+			transform.position = new Vector2(transform.position.x, EndChecker.endTop - moveRestOther);
+		}
+		else if (EndChecker.EndBottom(transform.position.y - moveRestOther))
+		{
+			transform.position = new Vector2(transform.position.x, EndChecker.endBottom + moveRestOther);
+		}
 	}
 }
--- Mirror_Fairies/Assets/Fairies/Hard/Black/HardBlackBurret.cs
+++ Mirror_Fairies/Assets/Fairies/Hard/Black/HardBlackBurret.cs
@@ -1,16 +1,49 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class HardBlackBurret : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
+﻿using UnityEngine;
+public class HardBlackBurret : MonoBehaviour
+{
+	public Vector2 target;
+	public float speed;
+	bool mirrorX;
+	bool mirrorY;
+	void Start()
+	{
+		transform.LookAt(target);
+		transform.rotation = Quaternion.Euler(0.0f, 0.0f, -transform.localEulerAngles.x);
+		mirrorX = false;
+		mirrorY = false;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	void Update()
+	{
+		transform.position += transform.right * Time.deltaTime * speed;
+		Mirror();
+		Destroy();
+	}
+	void Mirror()
+	{
+		if (!mirrorX && EndChecker.EndRight(transform.position.x))
+		{
+			transform.rotation = Quaternion.Euler(0.0f, 0.0f, 180.0f - transform.localEulerAngles.z);
+			mirrorX = true;
+		}
+		else if (mirrorX && !EndChecker.EndRight(transform.position.x))
+		{
+			mirrorX = false;
+		}
+		if (!mirrorY && EndChecker.EndTop(transform.position.y) || EndChecker.EndBottom(transform.position.y))
+		{
+			transform.rotation = Quaternion.Euler(0.0f, 0.0f, -transform.localEulerAngles.z);
+			mirrorY = true;
+		}
+		else if (mirrorY && !EndChecker.EndTop(transform.position.y) && !EndChecker.EndBottom(transform.position.y))
+		{
+			mirrorY = false;
+		}
+	}
+	void Destroy()
+	{
+		if (EndChecker.EndLeft(transform.position.x))
+		{
+			Destroy(gameObject);
+		}
 	}
 }
